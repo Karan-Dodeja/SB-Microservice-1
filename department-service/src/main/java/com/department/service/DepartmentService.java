1f10963e5d54e8d2e7aeea05065047897ba28175
kdodeja62@gmail.com <kdodeja62@gmail.com>
2023-05-06 03:40:11 +0530

    Add Department - Post Method Created

---
+++ department-service/src/main/java/com/department/service/DepartmentService.java
@@ -0,0 +1,21 @@
+package com.department.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.department.entity.Department;
+import com.department.repository.DepartmentRepository;
+
+@Service
+public class DepartmentService {
+	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+
+	public Department saveDepartment(Department deprtment) {
+		return departmentRepository.save(deprtment);
+	}
+	
+	
+	
+}
